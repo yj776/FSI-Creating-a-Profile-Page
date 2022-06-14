07242d5755fe4db86d2753b8ae2838033b92efa1
yj776 <yj776@nyu.edu>
2022-06-14 00:38:55 -0400

    saving

--- index.js
+++ index.js
@@ -19,4 +19,31 @@ let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
 
+let dogDescription = document.createElement ('h3')
+dogDescription.setAttribute('class','dog-description')
+content.append(dogDescription)
+
+dogDescription.textContent = 'Description'
+
+let dogDescripDetail = document.createElement ('p')
+dogDescripDetail.setAttribute('class','dog-Description')
+content.append(dogDescripDetail)
+
+dogDescripDetail.textContent = 'This Gentle Dog is aloof toward her owner'
+
+let feedTime = document.createElement ('h3')
+feedTime.setAttribute('class','feed-Time')
+content.append(feedTime)
+
+let feedingList = document.createElement ('ul')
+content.append(feedingList)
+
+let feedingTime1 = document.createElement ('li')
+feedingTime1.textContent='9:00 AM'
+let feedingTime2 = document.createElement ('li')
+feedingTime2.textContent='12:00 PM'
+let feedingTime3 = document.createElement ('li')
+feedingTime3.textContent='5:00 PM'
+
+feedingList.append(feedingTime1,feedingTime2,feedingTime3)
 
